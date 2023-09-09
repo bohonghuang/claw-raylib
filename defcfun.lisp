4462d348da794d9851b6605db6cffe8563e50398
coco24 <1281299809@qq.com>
2023-09-09 20:03:27 +0800

    Accept `cffi-object` as non-struct pointer argument by default

--- defcfun.lisp
+++ defcfun.lisp
@@ -3,10 +3,12 @@
 
 (in-package #:claw-raylib.defcfun)
 
+(defun cffi-pointer-type-p (type)
+  (and (typep (setf type (cffi::ensure-parsed-base-type type)) 'cffi::foreign-pointer-type) type))
+
 (defun cffi-object-type-p (type)
-  (and (typep (setf type (cffi::ensure-parsed-base-type type)) 'cffi::foreign-pointer-type)
-       (typep (setf type (cffi::ensure-parsed-base-type (cffi::pointer-type type))) 'cffi::foreign-struct-type)
-       (cffi::name type)))
+  (when-let ((type (cffi-pointer-type-p type)))
+    (and (typep (setf type (cffi::ensure-parsed-base-type (cffi::pointer-type type))) 'cffi::foreign-struct-type) type)))
 
 (defmacro defcobjfun (name result &rest args)
   (destructuring-bind (name symbol) name
@@ -19,17 +21,19 @@
          (export ',internal-symbol ',(symbol-package internal-symbol))
          ,(when should-define-wrapper-p
             (if return-object-p
-                (let* ((object-type (when return-object-p (cffi-object-type-p (cadar args))))
-                       (object-constructor (when return-object-p (intern (format nil "~A~A" '#:make- object-type) (symbol-package object-type)))))
+                (let* ((object-type (cffi-object-type-p (cadar args)))
+                       (object-constructor (if-let ((type-name (when object-type (cffi::name object-type))))
+                                             (intern (format nil "~A~A" '#:make- type-name) (symbol-package type-name))
+                                             (error "Defining a C function that returns non-structure pointer is currently not supported."))))
                   `(progn
                      (declaim (ftype function ,object-constructor)
                               (notinline ,object-constructor))
                      (defun ,symbol ,(mapcar #'car (cdr args))
                        (let ((,(caar args) (,object-constructor)))
                          (progn
-                           (,internal-symbol . ,(loop :for (name type) :in args :collect (if (cffi-object-type-p type) `(cobj:cobject-pointer ,name) name)))
+                           (,internal-symbol . ,(loop :for (name type) :in args :collect (if (cffi-pointer-type-p type) `(cobj:cobject-pointer ,name) name)))
                            ,(caar args))))))
                 `(defun ,symbol ,(mapcar #'car args)
-                   (,internal-symbol . ,(loop :for (name type) :in args :collect (if (cffi-object-type-p type) `(cobj:cobject-pointer ,name) name))))))))))
+                   (,internal-symbol . ,(loop :for (name type) :in args :collect (if (cffi-pointer-type-p type) `(cobj:cobject-pointer ,name) name))))))))))
 
 (shiftf (macro-function '%defcfun) (macro-function 'cffi:defcfun) (macro-function 'defcobjfun))
