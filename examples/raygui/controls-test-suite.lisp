2f5cb5d9e7c0d94f93baa6dd2c0d06aa514be027
coco24 <1281299809@qq.com>
2023-11-18 22:37:31 +0800

    Update the controls test suite example

--- examples/raygui/controls-test-suite.lisp
+++ examples/raygui/controls-test-suite.lisp
@@ -16,7 +16,7 @@
 (cobj:define-cobject-class (:struct cfloat))
 
 (defun controls-test-suite ()
-  (let ((screen-width 690)
+  (let ((screen-width 960)
         (screen-height 560))
     (raylib:with-window ("raygui - controls test suite" (screen-width screen-height))
       (raylib:set-exit-key 0)
@@ -30,6 +30,8 @@
             (value-box-edit-mode (make-cbool :value nil))
             (text-box-text (cobj:make-carray 64 :element-type 'character :initial-contents "Text box"))
             (text-box-edit-mode (make-cbool :value nil))
+            (text-box-multi-text (cobj:make-carray 1024 :element-type 'character :initial-contents (format nil "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.~%~%Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.~%~%Thisisastringlongerthanexpectedwithoutspacestotestcharbreaksforthosecases,checkingifworkingasexpected.~%~%Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")))
+            (text-box-multi-edit-mode (make-cbool :value nil))
             (list-view-scroll-index (make-cint :value 0))
             (list-view-active (make-cint :value -1))
             (list-view-ex-scroll-index (make-cint :value 0))
@@ -52,8 +54,8 @@
             (exit-window (make-cbool :value nil))
             (show-message-box (make-cbool :value nil))
             (text-input (cobj:make-carray 256 :element-type 'character :initial-element #\Null))
-            (show-text-input-box (make-cbool :value nil))
             (text-input-file-name (cobj:make-carray 256 :element-type 'character :initial-element #\Null))
+            (show-text-input-box (make-cbool :value nil))
             (alpha (make-cfloat :value 1.0))
             (secret-view-active (cobj:pointer-cobject (null-pointer) 'cbool)))
         (declare (ignore multi-text-box-text multi-text-box-edit-mode))
@@ -155,6 +157,10 @@
                         (raygui:unlock)
                         (raygui:set-style
                          #.(foreign-enum-value 'raygui:control :dropdownbox)
+                         #.(foreign-enum-value 'raygui:control-property :text-padding)
+                         4)
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :dropdownbox)
                          #.(foreign-enum-value 'raygui:control-property :text-alignment)
                          #.(foreign-enum-value 'raygui:text-alignment :left))
                         (when (plusp (raygui:dropdown-box
@@ -166,6 +172,10 @@
                          #.(foreign-enum-value 'raygui:control :dropdownbox)
                          #.(foreign-enum-value 'raygui:control-property :text-alignment)
                          #.(foreign-enum-value 'raygui:text-alignment :center))
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :dropdownbox)
+                         #.(foreign-enum-value 'raygui:control-property :text-padding)
+                         0)
                         (when (plusp (raygui:dropdown-box
                                       (raylib:make-rectangle :x 25.0 :y 25.0 :width 125.0 :height 30.0)
                                       "ONE;TWO;THREE"
@@ -183,14 +193,22 @@
                          (raylib:make-rectangle :x 165.0 :y 360.0 :width 140.0 :height 24.0)
                          (format nil "#1#ONE~%#3#TWO~%#8#THREE~%#23#")
                          toggle-group-active)
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :slider)
+                         #.(foreign-enum-value 'raygui:slider-property :padding)
+                         2)
                         (raygui:toggle-slider
                          (raylib:make-rectangle :x 165.0 :y 480.0 :width 140.0 :height 30.0)
                          "ON;OFF" toggle-slider-active)
-                        (raygui:panel (raylib:make-rectangle :x 320.0 :y 25.0 :width 225.0 :height 30.0) "Panel Info")
-                        (raygui:color-picker (raylib:make-rectangle :x 320.0 :y 185.0 :width 225.0 :height 140.0) (null-pointer) color-picker-value)
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :slider)
+                         #.(foreign-enum-value 'raygui:slider-property :padding)
+                         0)
+                        (raygui:panel (raylib:make-rectangle :x 320.0 :y 25.0 :width 225.0 :height 140.0) "Panel Info")
+                        (raygui:color-picker (raylib:make-rectangle :x 320.0 :y 185.0 :width 196.0 :height 192.0) (null-pointer) color-picker-value)
                         (raygui:slider (raylib:make-rectangle :x 355.0 :y 400.0 :width 165.0 :height 20.0) "TEST" (format nil "~,2F" (cfloat-value slider-value)) slider-value -50.0 100.0)
                         (raygui:slider-bar (raylib:make-rectangle :x 320.0 :y 430.0 :width 200.0 :height 20.0) (null-pointer) (format nil "~D" (floor (cfloat-value slider-bar-value))) slider-bar-value 0.0 100.0)
-                        (raygui:progress-bar (raylib:make-rectangle :x 320.0 :y 460.0 :width 200.0 :height 20.0) (null-pointer) (format nil " ~D%" (floor (* (cfloat-value progress-value) 100.0))) progress-value 0.0 1.0)
+                        (raygui:progress-bar (raylib:make-rectangle :x 320.0 :y 460.0 :width 200.0 :height 20.0) (null-pointer) (format nil "~D%" (floor (* (cfloat-value progress-value) 100.0))) progress-value 0.0 1.0)
                         (raygui:enable)
                         (let ((view (raylib:make-rectangle)))
                           (raygui:scroll-panel
@@ -200,13 +218,39 @@
                           (raygui:grid
                            (raylib:make-rectangle :x 560.0 :y (+ 25.0 180.0 195.0)
                                                   :width 100.0 :height 120.0)
-                           (null-pointer) 20.0 2 mouse-cell))
+                           (null-pointer) 20.0 3 mouse-cell))
+                        (raygui:color-bar-alpha (raylib:make-rectangle :x 320.0 :y 490.0 :width 200.0 :height 30.0) (null-pointer) alpha-value)
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :default)
+                         #.(foreign-enum-value 'raygui:default-property :text-alignment-vertical)
+                         #.(foreign-enum-value 'raygui:text-alignment-vertical :top))
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :default)
+                         #.(foreign-enum-value 'raygui:default-property :text-wrap-mode)
+                         #.(foreign-enum-value 'raygui:text-wrap-mode :word))
+                        (when (plusp (raygui:text-box (raylib:make-rectangle :x 678.0 :y 25.0 :width 258.0 :height 492.0) (& text-box-multi-text) 1024 (cbool-value text-box-multi-edit-mode)))
+                          (setf (cbool-value text-box-multi-edit-mode) (not (cbool-value text-box-multi-edit-mode))))
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :default)
+                         #.(foreign-enum-value 'raygui:default-property :text-wrap-mode)
+                         #.(foreign-enum-value 'raygui:text-wrap-mode :none))
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :default)
+                         #.(foreign-enum-value 'raygui:default-property :text-alignment-vertical)
+                         #.(foreign-enum-value 'raygui:text-alignment-vertical :middle))
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :default)
+                         #.(foreign-enum-value 'raygui:control-property :text-alignment)
+                         #.(foreign-enum-value 'raygui:text-alignment :left))
                         (raygui:status-bar
                          (raylib:make-rectangle :x 0.0 :y (- (coerce (raylib:get-screen-height) 'single-float) 20.0)
                                                 :width (coerce (raylib:get-screen-width) 'single-float)
                                                 :height 20.0)
                          "This is a status bar")
-                        (raygui:color-bar-alpha (raylib:make-rectangle :x 320.0 :y 490.0 :width 200.0 :height 30.0) (null-pointer) alpha-value)
+                        (raygui:set-style
+                         #.(foreign-enum-value 'raygui:control :default)
+                         #.(foreign-enum-value 'raygui:control-property :text-alignment)
+                         #.(foreign-enum-value 'raygui:text-alignment :center))
                         (when (cbool-value show-message-box)
                           (raylib:draw-rectangle 0 0 (raylib:get-screen-width) (raylib:get-screen-height) faded-raywhite)
                           (case (raygui:message-box
@@ -223,8 +267,8 @@
                                          (raylib:make-rectangle :x (coerce (- (floor (raylib:get-screen-width) 2) 120) 'single-float)
                                                                 :y (coerce (- (floor (raylib:get-screen-height) 2) 60) 'single-float)
                                                                 :width 240.0 :height 140.0)
-                                         "Save"
                                          (raygui:icon-text #.(foreign-enum-value 'raygui:icon-name :file-save) "Save file as...")
+                                         "Introduce output file name:"
                                          "Ok;Cancel" (& text-input) 255 secret-view-active)))
                             (case result
                               (1 (setf (cobj:carray-string text-input-file-name) (cobj:carray-string text-input))))
